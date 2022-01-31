3905d42e6e10348a320153cd4124975c9f1aa26a
sadak <kadas2003@gmail.com>
2022-01-31 18:21:05 +0100

    fix cache

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import shutil
+import os
 from pathlib import Path
-
 from setuptools import setup
-from simplify import SIMPLIFY_CACHE
 
+
+SIMPLIFY_CACHE = os.path.expanduser(os.path.join("~/.cache", "simplify"))
+if not os.path.exists(SIMPLIFY_CACHE):
+    os.mkdir(SIMPLIFY_CACHE)
 shutil.copy(Path(".jar/discourse.jar"), SIMPLIFY_CACHE)
 
 
